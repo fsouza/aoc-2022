11c8aab5f5ab8150843231b6b6e0d34e9095df09
francisco souza <108725+fsouza@users.noreply.github.com>
2022-12-01 09:38:25 -0500

    Day 1 part 2
    Not enough data to justify a heap. This is not an interview question,
    sorting is fine :)

--- day01/day1.go
+++ day01/day1.go
@@ -13,31 +13,56 @@ import (
 type elf []int
 
 func (e elf) total() int {
+	return sliceSum(e)
+}
+
+func sliceSum(s []int) int {
 	var sum int
-	for _, food := range e {
-		sum += food
+	for _, v := range s {
+		sum += v
 	}
 	return sum
 }
 
 func main() {
-	part1(os.Stdin)
+	elves := readElves(os.Stdin)
+	part1(elves)
+	part2(elves)
 }
 
-func part1(r io.Reader) {
+func readElves(r io.Reader) []elf {
+	var elves []elf
 	data, err := io.ReadAll(r)
 	if err != nil {
 		panic(err)
 	}
 	rawElves := strings.Split(string(data), "\n\n")
-	var largest int
 	for _, rawElf := range rawElves {
-		elf := parseElf(rawElf)
+		elves = append(elves, parseElf(rawElf))
+	}
+	return elves
+}
+
+func part1(elves []elf) {
+	var largest int
+	for _, elf := range elves {
 		if total := elf.total(); total > largest {
 			largest = total
 		}
 	}
-	fmt.Println(largest)
+	fmt.Printf("Part 1: %d\n", largest)
+}
+
+func part2(elves []elf) {
+	// this function could use a heap with only the top 3 elements, but
+	// given the volume of data, sorting is fine.
+	var totals []int
+	for _, elf := range elves {
+		totals = append(totals, elf.total())
+	}
+	slices.Sort(totals)
+	top3 := totals[len(totals)-3:]
+	fmt.Printf("Part 2: %d\n", sliceSum(top3))
 }
 
 func parseElf(input string) elf {
