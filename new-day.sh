a846545483da3b328d47227ba0dbeaa6b69b17bc
francisco souza <108725+fsouza@users.noreply.github.com>
2022-12-01 23:48:48 -0500

    Add a script for a new day
    Yes, Java, because I like to suffer. idk

---
+++ new-day.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+my_dir=$(cd "$(dirname "${0}")" && pwd)
+
+function find_last_day {
+	fd --exact-depth 1 --type d 'day\d\d' "${my_dir}" | sort | tail -1 | sd '^.+/day(\d+)/$' '$1'
+}
+
+function find_next_day {
+	local last_day
+	last_day=$(find_last_day)
+	printf "day%02d" $((last_day + 1))
+}
+
+function main {
+	local next_day
+	local dir
+	next_day=$(find_next_day)
+	dir=${my_dir}/${next_day}
+
+	echo "generating ${dir}"
+	mkdir -p "${dir}"
+	curl -sd type=java-application -d dsl=kotlin -d archive=tgz -d gradleVersion=7.6 -d projectName="${next_day}" -d packageName=dev.fsouza.aoc."${next_day}" https://gradle-initializr.cleverapps.io/starter | tar -C "${dir}" -xzf -
+	echo "success, created ${dir}"
+}
+
+main
