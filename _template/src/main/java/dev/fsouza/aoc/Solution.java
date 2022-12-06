0bc66eddbd7ece72f89bebc5b7d14335669e166b
francisco souza <108725+fsouza@users.noreply.github.com>
2022-12-05 23:56:51 -0500

    comment isPart2 declaration in the template
    I keep doing that manually anyways.

--- _template/src/main/java/dev/fsouza/aoc/Solution.java
+++ _template/src/main/java/dev/fsouza/aoc/Solution.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) {
-        final var isPart2 = args.length > 0 && args[0].equals("part2");
+        // final var isPart2 = args.length > 0 && args[0].equals("part2");
         final var reader = new BufferedReader(new InputStreamReader(System.in));
         final var lines = reader.lines();
     }
