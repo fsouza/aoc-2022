45d9dd48a14e11cbcdf92d35e10513421e96b477
francisco souza <108725+fsouza@users.noreply.github.com>
2022-12-06 00:08:53 -0500

    Day 6 part 1

--- day06/src/main/java/dev/fsouza/aoc/Solution.java
+++ day06/src/main/java/dev/fsouza/aoc/Solution.java
@@ -1,12 +1,24 @@
 package dev.fsouza.aoc;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Solution {
-    public static void main(String[] args) {
-        final var isPart2 = args.length > 0 && args[0].equals("part2");
+    public static void main(String[] args) throws IOException {
+        // final var isPart2 = args.length > 0 && args[0].equals("part2");
         final var reader = new BufferedReader(new InputStreamReader(System.in));
-        final var lines = reader.lines();
+        final var line = reader.readLine();
+
+        for (var i = 4; i < line.length(); i++) {
+            final Set<Character> chars = line.substring(i - 4, i).chars().mapToObj(ch -> (char) ch)
+                    .collect(Collectors.toSet());
+            if (chars.size() == 4) {
+                System.out.println(i);
+                break;
+            }
+        }
     }
 }
