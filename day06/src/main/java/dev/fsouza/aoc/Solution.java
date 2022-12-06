99df5c80e270eacd1bcd5b77c01f293f03c5566d
francisco souza <108725+fsouza@users.noreply.github.com>
2022-12-06 00:10:54 -0500

    day 6 part 2

--- day06/src/main/java/dev/fsouza/aoc/Solution.java
+++ day06/src/main/java/dev/fsouza/aoc/Solution.java
@@ -8,14 +8,15 @@ import java.util.stream.Collectors;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        // final var isPart2 = args.length > 0 && args[0].equals("part2");
+        final var isPart2 = args.length > 0 && args[0].equals("part2");
         final var reader = new BufferedReader(new InputStreamReader(System.in));
         final var line = reader.readLine();
+        final var messageLength = isPart2 ? 14 : 4;
 
-        for (var i = 4; i < line.length(); i++) {
-            final Set<Character> chars = line.substring(i - 4, i).chars().mapToObj(ch -> (char) ch)
+        for (var i = messageLength; i < line.length(); i++) {
+            final Set<Character> chars = line.substring(i - messageLength, i).chars().mapToObj(ch -> (char) ch)
                     .collect(Collectors.toSet());
-            if (chars.size() == 4) {
+            if (chars.size() == messageLength) {
                 System.out.println(i);
                 break;
             }
