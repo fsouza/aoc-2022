e34118c4e316bc40aa79edd5982564db708a8d8c
francisco souza <108725+fsouza@users.noreply.github.com>
2022-12-10 18:56:17 -0500

    day 8 part 2 wip

--- day08/src/main/kotlin/aoc/Solution.kt
+++ day08/src/main/kotlin/aoc/Solution.kt
@@ -7,29 +7,45 @@ import kotlin.streams.asSequence
 fun main() {
     val reader = BufferedReader(InputStreamReader(System.`in`))
     val grid =
-            reader.lineSequence().map { it.asSequence().map { it.digitToInt() }.toList() }.toList()
+        reader.lineSequence().map { it.asSequence().map { it.digitToInt() }.toList() }.toList()
+
+    println("Part 1: ${calcPart1(grid)}")
+    println("Part 2: ${calcPart2(grid)}")
+}
+
+fun calcPart1(grid: List<List<Int>>): Int {
     val rows = grid.size
     val cols = grid[0].size
-    var count = 0
 
-    for (i in 0 until rows) {
-        for (j in 0 until cols) {
-            val tree = grid[i][j]
+    return indices(grid)
+        .map {
+            val tree = grid[it.first][it.second]
 
-            if (i == 0 || tree > maxOnNorth(grid, i, j)) {
-                count++
-            } else if (j == cols - 1 || tree > maxOnEast(grid, i, j)) {
-                count++
-            } else if (i == rows - 1 || tree > maxOnSouth(grid, i, j)) {
-                count++
-            } else if (j == 0 || tree > maxOnWest(grid, i, j)) {
-                count++
+            when {
+                it.first == 0 || tree > maxOnNorth(grid, it.first, it.second) -> 1
+                it.second == cols - 1 || tree > maxOnEast(grid, it.first, it.second) -> 1
+                it.first == rows - 1 || tree > maxOnSouth(grid, it.first, it.second) -> 1
+                it.second == 0 || tree > maxOnWest(grid, it.first, it.second) -> 1
+                else -> 0
             }
         }
-    }
-
-    println(count)
+        .sum()
 }
+
+fun calcPart2(grid: List<List<Int>>) =
+    indices(grid).map { scenicScore(grid, it.first, it.second) }.max()
+
+fun indices(grid: List<List<Int>>): Sequence<Pair<Int, Int>> {
+    val rows = grid.size
+    val cols = grid[0].size
+
+    return generateSequence(Pair(0, 0)) { (row, col) ->
+        if (row == rows - 1 && col == cols - 1) null
+        else if (col == cols - 1) Pair(row + 1, 0) else Pair(row, col + 1)
+    }
+}
+
+fun max(x: Int, y: Int) = if (x > y) x else y
 
 fun maxOnNorth(grid: List<List<Int>>, row: Int, col: Int) =
     grid.subList(0, row).asSequence().map { it[col] }.max()
@@ -40,5 +56,47 @@ fun maxOnEast(grid: List<List<Int>>, row: Int, col: Int) =
 fun maxOnSouth(grid: List<List<Int>>, row: Int, col: Int) =
     grid.subList(row + 1, grid.size).asSequence().map { it[col] }.max()
 
-fun maxOnWest(grid: List<List<Int>>, row: Int, col: Int) =
-    grid[row].subList(0, col).max()
+fun maxOnWest(grid: List<List<Int>>, row: Int, col: Int) = grid[row].subList(0, col).max()
+
+fun scenicScore(grid: List<List<Int>>, row: Int, col: Int): Int {
+    val tree = grid[row][col]
+    val rows = grid.size
+    val cols = grid[0].size
+
+    val northIndices =
+        generateSequence(row - 1) { if (it > 0) it - 1 else null }
+            .filter { it >= 0 }
+            .takeWhile { grid[it][col] < tree }
+            .toList()
+    val eastIndices =
+        generateSequence(col + 1) { if (it < cols - 1) it + 1 else null }
+            .filter { it < cols }
+            .takeWhile { grid[row][it] < tree }
+            .toList()
+    val southIndices =
+        generateSequence(row + 1) { if (it < rows - 1) it + 1 else null }
+            .filter { it < rows }
+            .takeWhile { grid[it][col] < tree }
+            .toList()
+    val westIndices =
+        generateSequence(col - 1) { if (it > 0) it - 1 else null }
+            .filter { it >= 0 }
+            .takeWhile { grid[row][col] < tree }
+            .toList()
+
+    val northValue =
+        if (northIndices.size > 0 && northIndices.last() > 0) northIndices.size + 1
+        else northIndices.size
+    val eastValue =
+        if (eastIndices.size > 0 && eastIndices.last() < cols - 1) eastIndices.size + 1
+        else eastIndices.size
+    val southValue =
+        if (southIndices.size > 0 && southIndices.last() < rows - 1) southIndices.size + 1
+        else southIndices.size
+    val westValue =
+        if (westIndices.size > 0 && westIndices.last() > 0) westIndices.size + 1 else westIndices.size
+
+    return sequenceOf(northValue, eastValue, southValue, westValue)
+        .filter { it > 0 }
+        .fold(1) { x, y -> x * y }
+}
