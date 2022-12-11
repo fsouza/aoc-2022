ada71ef941dbecd61f59311abcac865f0642d63c
francisco souza <108725+fsouza@users.noreply.github.com>
2022-12-10 21:19:44 -0500

    Skeleton for day 9

---
+++ day09/src/main/kotlin/aoc/Solution.kt
@@ -0,0 +1,15 @@
+package dev.fsouza.aoc
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+class Solution {
+    fun run() {
+        val reader = BufferedReader(InputStreamReader(System.`in`))
+        val lines = reader.lineSequence()
+    }
+}
+
+fun main() {
+    Solution().run()
+}
